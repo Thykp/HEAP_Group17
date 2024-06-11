760799054baa8f023eca490a20a8a0ef5f55e10d
ch00nki <ryankck123@gmail.com>
2024-06-11 22:35:42 +0800

    Minor changes to navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero";
 import Taskbar from "./components/Taskbar";
 import LineGraph from "./components/LineGraph";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
@@ -19,7 +20,8 @@ const App = () => {
 
       <ButtonGradient /> */}
 
-      <Login />
+      {/* <Login /> */}
+      <Home/>
     </>
   );
 };
