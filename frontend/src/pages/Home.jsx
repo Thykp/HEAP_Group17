3446abc659be920de62e6bb50a6519db49ee6106
Thykp <101046365+Thykp@users.noreply.github.com>
2024-06-17 00:41:54 +0800

    added skeletal Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,17 +3,13 @@ import Taskbar from '../components/Taskbar.jsx'
 import LineGraph from '../components/LineGraph.jsx'
 import Navbar from '../components/Navbar.jsx'
 import Header from '../components/Header.jsx'
+import Dashboard from '../components/Dashboard.jsx'
 
 const Home = () => {
   
-  const workouts = ['Pushups', 'Squats', 'Planks', 'Jumping Jacks'];
-  const data = [20,30,45,50,60,70,95,150,160,165,170,200];
-
   return (
     <div>
-      <Navbar/>
-      <Taskbar workouts={workouts} />
-      <LineGraph data={data}/>
+      <Dashboard />
     </div>
   )
 }
