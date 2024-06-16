aad2450b0b08435196de4eff0f3d9e9dad709bf0
Thykp <101046365+Thykp@users.noreply.github.com>
2024-06-17 00:41:54 +0800

    added skeletal Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Landing from "./pages/Landing";
 
 const App = () => {
+  const location = useLocation();
+
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Landing />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/home" element={<Home />}/>
-      </Routes>
-    </>
+    <TransitionGroup>
+      <CSSTransition
+        key={location.key}
+        classNames="page"
+        timeout={700}
+      >
+        <Routes location={location}>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
--- frontend/src/pages/Nutrition.jsx
+++ frontend/src/pages/Nutrition.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Nutrition = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center justify-center h-screen bg-n-8 text-n-1 p-6">
+      <h1 className="text-4xl font-bold text-color-1 mb-6">Nutrition</h1>
+      <p className="text-lg text-n-2">
+        Detailed information about Nutrition goes here...
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Nutrition
+export default Nutrition;
