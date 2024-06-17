7e76b3918d76f74766d0ea4b32c0ce083fb4d4d2
Thykp <101046365+Thykp@users.noreply.github.com>
2024-06-17 21:08:29 +0800

    added profile and updated background rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,29 @@
+// App.jsx
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Landing from "./pages/Landing";
+import Profile from "./pages/Profile";
+import '../src/index.css';
+import Layout from './components/Layout';
 
 const App = () => {
   const location = useLocation();
 
   return (
-    <TransitionGroup>
-      <CSSTransition
-        key={location.key}
-        classNames="page"
-        timeout={700}
-      >
-        <Routes location={location}>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </CSSTransition>
-    </TransitionGroup>
+    <Layout>
+      <TransitionGroup>
+        <CSSTransition key={location.key} classNames="page" timeout={500}>
+          <Routes location={location}>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
+    </Layout>
   );
 };
 
