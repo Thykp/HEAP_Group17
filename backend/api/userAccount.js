1bf709fd56b4bf250ce5739c3ceca72c7fe7746a
Thykp <101046365+Thykp@users.noreply.github.com>
2024-06-28 19:34:19 +0800

    added new packages

--- backend/api/userAccount.js
+++ backend/api/userAccount.js
@@ -5,14 +5,15 @@ const userAccount = require("../model/userAccount");
 router.get('/', async (req, res) => {
 
     try {
-        const { userName, userPassword } = req.body;
 
-        const login = await userAccount.getUserAccount(userName, userPassword);
-        if (!login) {
-            return res.status(400).json({ "error": "Account already something!" });
+        const { username, password } = req.body;
+        const account = await userAccount.getUserAccount(username, password);
+
+        if (!(account.length > 0)) {
+            return res.status(400).json({ "error": "Account does not exist!" });
         }
 
-        res.status(200);
+        res.status(200).json({'message': 'Account retrieved'});
         
     } catch (error) {
         res.status(500).json({ error: error.message });
