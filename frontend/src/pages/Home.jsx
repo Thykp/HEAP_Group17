b480de7aae2aceaf3daae51bbacdaa08b20ea68b
Thykp <101046365+Thykp@users.noreply.github.com>
2024-07-12 19:32:22 +0800

    restarted my directory cuz i fked up

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import Taskbar from '../components/Taskbar.jsx'
+import LineGraph from '../components/LineGraph.jsx'
+import Navbar from '../components/Navbar.jsx'
+import Header from '../components/Header.jsx'
+import Dashboard from '../components/Dashboard.jsx'
+
+const Home = () => {
+  
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
+
+export default Home
